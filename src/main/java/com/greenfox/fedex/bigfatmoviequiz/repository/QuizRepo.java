3b6c15cabbac354e6040022ec371b424f1fc8135
pkrisz0 <p.krisz89@gmail.com>
2018-01-10 16:46:57 +0100

    next and back buttons

--- src/main/java/com/greenfox/fedex/bigfatmoviequiz/repository/QuizRepo.java
+++ src/main/java/com/greenfox/fedex/bigfatmoviequiz/repository/QuizRepo.java
@@ -1,8 +1,10 @@
 package com.greenfox.fedex.bigfatmoviequiz.repository;
 
 import com.greenfox.fedex.bigfatmoviequiz.model.Quiz;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface QuizRepo extends CrudRepository<Quiz, Long> {
-    Quiz findById(Long id);
+public interface QuizRepo extends JpaRepository<Quiz, Long> {
+    Quiz findById(Long id, PageRequest pageRequest);
 }
