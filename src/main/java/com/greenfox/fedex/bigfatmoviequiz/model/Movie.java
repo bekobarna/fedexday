ed660d2737b3ba7733b1870ffae09f14e36d104d
bekobarna <bocce15@gmail.com>
2018-01-11 11:50:51 +0100

    Movie with images fields

--- src/main/java/com/greenfox/fedex/bigfatmoviequiz/model/Movie.java
+++ src/main/java/com/greenfox/fedex/bigfatmoviequiz/model/Movie.java
@@ -15,18 +15,24 @@ public class Movie {
     String wrongTitleOne;
     String wrongTitleTwo;
     String imdbID;
-    String imageURL;
+    String correctImage;
+    String wrong1Image;
+    String wrong2Image;
 
     public Movie() {
     }
 
-    public Movie(String description, String correctTitle, String wrongTitleOne, String wrongTitleTwo, String imdbID, String imageURL) {
+    public Movie(String description, String correctTitle, String wrongTitleOne,
+        String wrongTitleTwo, String imdbID, String correctImage, String wrong1Image,
+        String wrong2Image) {
         this.description = description;
         this.correctTitle = correctTitle;
         this.wrongTitleOne = wrongTitleOne;
         this.wrongTitleTwo = wrongTitleTwo;
         this.imdbID = imdbID;
-        this.imageURL = imageURL;
+        this.correctImage = correctImage;
+        this.wrong1Image = wrong1Image;
+        this.wrong2Image = wrong2Image;
     }
 
     public String getDescription() {
@@ -69,19 +75,35 @@ public class Movie {
         this.imdbID = imdbID;
     }
 
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getCorrectImage() {
+        return correctImage;
+    }
+
+    public void setCorrectImage(String correctImage) {
+        this.correctImage = correctImage;
+    }
+
+    public String getWrong1Image() {
+        return wrong1Image;
+    }
+
+    public void setWrong1Image(String wrong1Image) {
+        this.wrong1Image = wrong1Image;
+    }
+
+    public String getWrong2Image() {
+        return wrong2Image;
+    }
+
+    public void setWrong2Image(String wrong2Image) {
+        this.wrong2Image = wrong2Image;
     }
 }
